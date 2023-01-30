6e861ce0fcd6b2d255bc4d6c838e72148e4516e8
nickmurph <nfmurphy@buffalo.edu>
2023-01-30 15:00:16 -0700

    modified removeHead()

--- DoublyLinkedList/doubly_linked_list.js
+++ DoublyLinkedList/doubly_linked_list.js
@@ -19,6 +19,7 @@ export default class DoublyLinkedList {
         if (this.head){
             let oldHead = this.head;
             this.head = this.head.next;
+            this.head.prev = null;
             return oldHead;
         }
         return null;
