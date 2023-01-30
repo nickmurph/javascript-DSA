38087a27188ec4320f82548e0fc164ae4d4e92d8
nickmurph <nfmurphy@buffalo.edu>
2023-01-30 16:00:41 -0700

    isEmpty() implemented and used for error checking in other methods

--- Stack/stack.js
+++ Stack/stack.js
@@ -15,19 +15,22 @@ export default class Stack {
 
     // removes the top item from the stack and returns it
     // the underlying LinkedList returns its removed head node, we return that nodes data field
+    // if the stack is empty, return null
     pop(){
-        return this.stack.removeHead().data;
+        this.size > 0 ? this.size-- : this.size = 0;
+        return this.isEmpty() ? null : this.stack.removeHead().data;
     };
 
     // returns the top item from the stack without removing it
     // access the head node of the underlying stack and return its data field
+    // if the stack is empty, return null
     peak(){
-        return this.stack.head.data;
+        return this.isEmpty() ? null : this.stack.head.data;
     };
 
     // returns true if the stack is empty, false if it has any number of items
     isEmpty(){
-
+        return (this.stack.head === null)
     };
 
     // deletes all items from the stack
