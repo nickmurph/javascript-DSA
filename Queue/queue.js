5fd5df45e8e28030b7d36a7a6abe1d2a0b548c1f
nickmurph <nfmurphy@buffalo.edu>
2023-01-30 16:43:59 -0700

    Queue method comments

--- Queue/queue.js
+++ Queue/queue.js
@@ -7,22 +7,28 @@ export default class Queue {
         this.size = (firstItem ? 1 : 0 );
     };
 
+    // add the item to the queue, using append on the underlying linked list
     enqueue(item){
 
     };
 
+    // remove the next item in the queue, in this case this is the tail of the underlying linked list
+    // return the item after removing it
     dequeue(){
 
     };
 
+    // return the next item in the queue without removing it
     peek(){
 
     };
 
+    // return true if the queue is empty, false if it has any number of items
     isEmpty(){
 
     };
 
+    // delete all the items in the queue
     clear(){
 
     };
